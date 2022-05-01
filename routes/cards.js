a7908b58361aa28de264f7811c85b41e5987f2c7
Timur <thedickst@gmail.com>
2022-05-01 19:42:14 +0300

    new fix

--- routes/cards.js
+++ routes/cards.js
@@ -3,17 +3,36 @@
 /* eslint-disable import/no-unresolved */
 
 const router = require('express').Router();
-
+const { celebrate, Joi } = require('celebrate');
+const { validateUrl } = require('../utils/validationUrl');
 const { getAllCards, deleteCardId, createCard, likeCard, dislikeCard } = require('../controllers/cards');
 
 router.get('/cards', getAllCards);
 
-router.post('/cards', createCard);
+router.post('/cards', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2)
+      .max(30),
+    link: Joi.string().min(2).required().custom(validateUrl),
+  }),
+}), createCard);
 
-router.delete('/cards/:cardId', deleteCardId);
+router.delete('/cards/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+}), deleteCardId);
 
-router.put('/cards/:cardId/likes', likeCard);
+router.put('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+}), likeCard);
 
-router.delete('/cards/:cardId/likes', dislikeCard);
+router.delete('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+}), dislikeCard);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,17 +3,31 @@
 /* eslint-disable import/no-unresolved */
 
 const userRouter = require('express').Router();
-
+const { celebrate, Joi } = require('celebrate');
+const { validateUrl } = require('../utils/validationUrl');
 const { getAllUsers, getUserId, updateProfileUser, updateAvatarUser, getUserMe } = require('../controllers/users');
 
-userRouter.get('/users/:userId', getUserId);
+userRouter.get('/users/:userId', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().length(24).hex().required(),
+  }),
+}), getUserId);
 
 userRouter.get('/users/', getUserMe);
 
 userRouter.get('/users', getAllUsers);
 
-userRouter.patch('/users/me', updateProfileUser);
+userRouter.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateProfileUser);
 
-userRouter.patch('/users/me/avatar', updateAvatarUser);
+userRouter.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().custom(validateUrl),
+  }),
+}), updateAvatarUser);
 
 module.exports = userRouter;
