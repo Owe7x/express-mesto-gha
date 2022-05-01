d9f56542135c1adf63c08bfe8426abdd94c25c18
Timur <thedickst@gmail.com>
2022-05-01 18:49:38 +0300

    new fix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const userRouter = require('./routes/users');
 
@@ -16,6 +17,12 @@ const app = express();
 app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(require('./routes/auth'));
 
 app.use(auth);
 
