62ff0952d228bf29d9f4a61693eb24efb5f98f2e
Timur <thedickst@gmail.com>
2022-05-02 18:37:49 +0300

    new fix

--- models/user.js
+++ models/user.js
@@ -1,43 +1,33 @@
 const validator = require('validator');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const { validateUrl } = require('../utils/validationUrl');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: false,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: false,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: false,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-    validate: {
-      validator(v) {
-        return validator.isURL(v);
-      },
-      message: 'Некорректный URL',
-
-    },
+    validate: { validator: validateUrl, message: 'Введите валидную ссылку' },
   },
   email: {
     type: String,
     required: true,
     unique: true,
     validate: {
-      validator(v) {
-        return validator.isEmail(v);
-      },
-      message: 'Некорректный email',
+      validator: (v) => validator.isEmail(v),
+      message: 'Неправильный формат почты',
     },
   },
   password: {
