4b48504d2b1e9f658f675c23964a3ea76c0729f9
Timur <thedickst@gmail.com>
2022-05-19 22:50:55 +0300

    fix cors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { celebrate, Joi } = require('celebrate');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-const cors = require('cors');
+/* const cors = require('cors'); */
 const { login, createUser } = require('./controllers/users');
 const userRouter = require('./routes/users');
 const auth = require('./middlewares/auth');
@@ -20,10 +20,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
 
-app.use(cors({
-  origin: 'http://owe7x.nomoredomains.xyz',
-  credentials: true,
-}));
+const allowedCors = [
+  'http://owe7x.nomoredomains.xyz',
+  'https://owe7x.nomoredomains.xyz',
+  'localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+});
 
 app.get('/crash-test', () => {
   setTimeout(() => {
